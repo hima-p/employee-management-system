b043ec369db8cf327d0a6cf3ba5860c49513caf0
Hima P <himapgopinath@gmail.com>
2025-07-12 21:45:19 +0530

    Add employee service and department service

---
+++ department-service/src/test/java/com/ems/department_service/DepartmentServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ems.department_service;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Disabled
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ employee-service/src/test/java/com/ems/employee_service/EmployeeServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ems.employee_service;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceApplicationTests {
+
+	@Disabled
+	@Test
+	void contextLoads() {
+	}
+
+}
